b6180cb35f5568a635cff1d295746644fbfc5841
chandra Prakash <engg.chandraprakash01@gmail.com>
2026-07-26 23:51:04 +0530

    .

--- src/main/java/com/TradeX/repository/TwoFactorOtpRepository.java
+++ src/main/java/com/TradeX/repository/TwoFactorOtpRepository.java
@@ -1,5 +1,9 @@
 package com.TradeX.repository;
 
-public class TwoFactorOtpRepository {
+import com.TradeX.modal.TwoFactorOTP;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TwoFactorOtpRepository extends JpaRepository<TwoFactorOTP,String> {
+    TwoFactorOTP findByUserId(long  userId);
 
 }
--- src/main/java/com/TradeX/service/TwoFactorServiceImpl.java
+++ src/main/java/com/TradeX/service/TwoFactorServiceImpl.java
@@ -1,12 +1,33 @@
 package com.TradeX.service;
 
 import com.TradeX.modal.TwoFactorOTP;
+import com.TradeX.repository.TwoFactorOtpRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+@Service
 public class TwoFactorServiceImpl implements TwoFactorOtpService {
+
+    @Autowired
+    private TwoFactorOtpRepository twoFactorOtpRepository;
+
     @Override
     public TwoFactorOTP createTwoFactorOtp(User user, String otp, String jwt) {
-        return null;
+        UUID uuid = UUID.randomUUID();
+        String id = uuid.toString();
+        TwoFactorOTP twoFactorOtp = new TwoFactorOTP();
+        twoFactorOtp.setId(id);
+        twoFactorOtp.setOtp(otp);
+        twoFactorOtp.setJwt(jwt);
+        twoFactorOtp.setUser(user);
+         return twoFactorOtpRepository.save(twoFactorOtp);
+
+
+
+
     }
 
     @Override
